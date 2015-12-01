ad204d0b4e92a14cced07b01901374edbf9ffda8
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-01 21:11:53 +0800

    Change static directory to dist

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -109,9 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dist/'
 
-STATIC_ROOT = normpath(join(BASE_DIR, 'static'))
+STATIC_ROOT = normpath(join(BASE_DIR, 'dist'))
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
