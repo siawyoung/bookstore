373a172535db60e0b428500b1036decd97cf9504
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-02 01:44:04 +0800

    Add book url route matcher

--- bookstore_app/urls.py
+++ bookstore_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/', views.register, name='register'),
-    url(r'^login/', views.login, name='login')
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^books/([a-zA-Z0-9]+)/$', views.book, name='book')
 ]
