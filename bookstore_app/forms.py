7bbf00a1e68d2310346c10f416e9bee16e99bb44
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-03 21:17:37 +0800

    Add RegisterForm

--- bookstore_app/forms.py
+++ bookstore_app/forms.py
@@ -1,13 +1,23 @@
+import pdb
 from django import forms
 from models import *
 
 class LoginForm(forms.ModelForm):
-    # username = forms.CharField(label='Username', max_length=20)
-    # password = forms.PasswordInput()
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Customer
         fields = ['login_id', 'password']
         labels = {
             'login_id': 'Username'
+        }
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        labels = {
+            'login_id': 'Username',
+            'cc_num': 'Credit Card',
+            'phone_num': 'Phone Number'
         }
