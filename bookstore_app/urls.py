13485e88dbbaf773e9b38b2626203a8b8973186b
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-03 22:06:04 +0800

    Remove id capture group from users URL regex

--- bookstore_app/urls.py
+++ bookstore_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', LoginView.as_view()),
     url(r'^books/([a-zA-Z0-9]+)/$', BookView.as_view()),
-    url(r'^users/(?P<user_id>\w+)/$', UserView.as_view()),
+    url(r'^users/$', UserView.as_view()),
     # url(r'^admin/book/new', views.admin_books, name='admin_books'),
     # url(r'^admin/book/update', views.admin_update, name='admin_update')
 ]
