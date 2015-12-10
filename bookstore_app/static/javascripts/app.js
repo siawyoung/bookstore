cc37d27d6725cdd5b6a870ea50635601d2103446
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-11 01:47:16 +0800

    Add sorting of books

--- bookstore_app/static/javascripts/app.js
+++ bookstore_app/static/javascripts/app.js
@@ -18,8 +18,39 @@ var attachFeedbackShowHandler = function() {
   })
 }
 
+var sort_year = function(a, b) {
+  return parseInt($(b).data('year')) < parseInt($(a).data('year')) ? 1 : -1;
+}
+
+var sort_year_r = function(a, b) {
+  return parseInt($(b).data('year')) > parseInt($(a).data('year')) ? 1 : -1;
+}
+
+var sort_score = function(a, b) {
+  return parseInt($(b).data('rating')) < parseInt($(a).data('rating')) ? 1 : -1;
+}
+
+var sort_score_r = function(a, b) {
+  return parseInt($(b).data('rating')) > parseInt($(a).data('rating')) ? 1 : -1;
+}
+
+var attachSortBooksFilter = function() {
+  $('#sort-dropdown').change(function() {
+    var sort = this.value
+    if (sort == 'year-a') {
+      $('.books a').sort(sort_year).appendTo('.books')
+    } else if (sort == 'year-d') {
+      $('.books a').sort(sort_year_r).appendTo('.books')
+    } else if (sort == 'score-a') {
+      $('.books a').sort(sort_score).appendTo('.books')
+    } else if (sort == 'score-d') {
+      $('.books a').sort(sort_score_r).appendTo('.books')
+    }
+  })
+}
 
 $(document).ready(function() {
   attachLogOutHandler()
   attachFeedbackShowHandler()
+  attachSortBooksFilter()
 })
