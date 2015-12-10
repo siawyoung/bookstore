e96ef5a2a9bf253f510265c784cf91a1cac389bf
Lau Siaw Young <lausiawyoung@gmail.com>
2015-12-11 01:17:44 +0800

    Add feedback number filter

--- bookstore_app/static/javascripts/app.js
+++ bookstore_app/static/javascripts/app.js
@@ -1,16 +1,25 @@
-
 var delete_cookie = function(name) {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;'
 };
 
 var attachLogOutHandler = function() {
-  var logOutButton = document.getElementById('logout-link');
+  $('#logout-link').click(function() {
+    delete_cookie('bookstore_token')
+  })
+}
 
-  if (logOutButton) {
-    logOutButton.addEventListener("click", function() {
-      delete_cookie('bookstore_token')
-    })
-  }
+var attachFeedbackShowHandler = function() {
+  $('#feedback-number-show').keyup(function() {
+    var input = parseInt(this.value);
+    if (input) {
+      $('div.feedback').hide()
+      $('div.feedback:lt(' + input + ')').show()
+    }
+  })
 }
 
 
+$(document).ready(function() {
+  attachLogOutHandler()
+  attachFeedbackShowHandler()
+})
