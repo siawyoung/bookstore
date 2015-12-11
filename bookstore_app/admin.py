791ec535f3c19e6b1d35777946b22caa7c8e9f7c
Glen <glencbz@gmail.com>
2015-12-11 15:25:12 +0800

    added recommendation

--- bookstore_app/admin.py
+++ bookstore_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Customer, Feedback, Rating
+from .models import Customer, Feedback, Rating, Order, Order_book
 
 admin.site.register(Customer)
 admin.site.register(Feedback)
 admin.site.register(Rating)
+admin.site.register(Order)
+admin.site.register(Order_book)
